560b3b2ec920bc35f9b482984fe9ea555232ceff
zack-at-tilden <63805939+zack-at-tilden@users.noreply.github.com>
2021-03-24 13:25:47 -0700

    update

--- database.py
+++ database.py
@@ -1,2 +1,17 @@
 # database.py
 #
+
+class Database:
+	
+	def __init__(self,idNumber,name,description,north,south,east,west,items):
+		self.id=idNumber
+		self.name=name
+		self.description=description
+		self.north=north
+		self.south=south
+		self.east=east
+		self.west=west
+		self.items=times
+		
+		
+	
