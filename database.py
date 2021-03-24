bc45ef2f9a36e45743f1165a1f9b1b68d4026ae5
unknown <63805939+zack-at-tilden@users.noreply.github.com>
2021-03-24 13:36:58 -0700

    Update database and room files

--- database.py
+++ database.py
@@ -3,15 +3,8 @@
 
 class Database:
 	
-	def __init__(self,idNumber,name,description,north,south,east,west,items):
-		self.id=idNumber
-		self.name=name
-		self.description=description
-		self.north=north
-		self.south=south
-		self.east=east
-		self.west=west
-		self.items=times
+	def __init__
+	
 		
 		
 	
---
+++ room.py
@@ -0,0 +1,14 @@
+# room.py
+#
+
+class Room:
+	def __init__(self,idNumber,name,description,north,south,east,west,items):
+		self.id=idNumber
+		self.name=name
+		self.description=description
+		self.north=north
+		self.south=south
+		self.east=east
+		self.west=west
+		self.items=times
+		
